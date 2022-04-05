62735fda666042d084820dce64e81968092b4f14
Javier-Azahel-Gutierrez <54181729+Javier-Azahel-Gutierrez@users.noreply.github.com>
2022-04-04 18:26:29 -0700

    Create paramtrizadoDSL.groovy

---
+++ paramtrizadoDSL.groovy
@@ -0,0 +1,41 @@
+job('ejemplo2-job-DSL'){
+	description('Job DSL de ejemplo 2 para el curso de Jenkins')
+	scm {
+        git('https://github.com/macloujulian/jenkins.job.parametrizado.git','main') { node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Javier-Azahel-Gutierrez')
+            node / gitConfigEmail('a219209037@unison.mx')
+        }
+    }
+  parameters {
+        stringParam('nombre', defaultValue = 'Javier', description = 'Parametro de cadena para el Job Booleano')
+    	  choiceParam('planeta', ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+        booleanParam('agente', false)
+    }
+    triggers {
+      	cron ( 'H/7 * * * *')
+    }
+    steps {
+      	shell(" jobscript.sh")
+    }
+  	publishers{
+    	mailer('a219209037@gmail.com', true, true)
+      	slackNotifier {
+			  notifyAborted(true)
+		  	notifyEveryFailure(true)
+		  	notifyNotBuilt(false)
+		  	notifyUnstable(false)
+		  	notifyBackToNormal(true)
+		  	notifySuccess(false)
+		  	notifyRepeatedFailure(false)
+		  	startNotification(false)
+		  	includeTestSummary(false)
+		  	includeCustomMessage(false)
+		  	customMessage(null)
+		  	sendAs(null)
+		  	commitInfoChoice('NONE')
+		  	teamDomain(null)
+		  	authToken(null)
+        }
+  	}
+                                  
+}
